db0057aa7173ed9103e8a4d4ce03254b38a54818
Chentille <chentillewongweize03@gmail.com>
2024-11-16 15:48:18 +0800

    Add Health Bar

---
+++ Custom Resources/Items/Item.gd
@@ -0,0 +1,15 @@
+class_name Item
+extends Resource
+
+
+@export var name:String
+@export var id:String
+@export var experience:int
+@export var description:String
+@export var pickup_sound:AudioStreamWAV
+@export var artwork:CompressedTexture2D
+@export var plastic_type:String
+#@export var graphics:Sprite2D
+@export var object:PackedScene
+
+
---
+++ Custom Resources/Projectiles/Projectile_logic.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+# THIS USES A CUSTOM RESOURCE  "Projectiles.gd"
+@export var Projectile:Projectiles
+@export var speed:float
+@onready var sfx = $AudioStreamPlayer2D
+@onready var artwork = $CollisionShape2D/Sprite2D
+@onready var hitbox = $CollisionShape2D
+
+# THIS INITIALIZES THE OBJECT PROPERTIES WHEN LOADED INTO THE GAME
+func _ready():
+	sfx.stream = Projectile.projectile_sound
+	artwork.texture = Projectile.artwork
+	sfx.play()
+	set_as_top_level(true)
+
+# THIS HANDLES THE PROJECTILE MOTION WHEN THROWN BY PLAYER
+func _physics_process(delta):
+	
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+	hitbox.rotation += 30*delta
+	
+	
+# THIS WILL DESTROY THE PROJECTILE WHEN IT HITS AN ENEMY
+func projectile_damage():
+	
+	print("hit!")
+	queue_free()
+
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
+
+
+func _on_body_entered(body):
+	if body.has_method("enemy"):
+		projectile_damage()
